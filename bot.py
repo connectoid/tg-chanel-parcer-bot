b2dac6b029e6e1d18be9d8fdbdef20cbf8a008f9
Alexander Beley <beleysasha@gmail.com>
2024-11-03 23:59:16 +1200

    message limit added

--- bot.py
+++ bot.py
@@ -75,6 +75,7 @@ async def process_request_articles_answer(message: Message):
         article_source_url = article.source_url
         image_urls_list = article.image_urls.split(', ')
         text = f'{article_header}\n\n{article_text_short}\n\n{message_footer}\n\n{break_line}\n\n{article_source_url}'
+        text = text[:1024]
 
         media_group = MediaGroupBuilder(caption=text)
         for image_url in image_urls_list:
